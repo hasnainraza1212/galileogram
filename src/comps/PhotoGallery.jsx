279db7fa3e6c07c948c7164c01c53d7d33e429d0
hasnainraza1212 <hr961992@gmail.com>
2023-08-14 15:27:10 +0500

    fireStore startAfter working

--- src/comps/PhotoGallery.jsx
+++ src/comps/PhotoGallery.jsx
@@ -1,35 +1,19 @@
 import '.././index.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { UseFirestore } from '../hooks/UseFirestore';
-useEffect
 const PhotoGallery = () => {
-  let [limiter, limiterUpdate] = useState(20);
-  
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const totalHeight = document.documentElement.scrollHeight -200;
-  //     const currentPosition = window.scrollY + window.innerHeight;
-  //     if (currentPosition >= totalHeight) {
-  //       limiterUpdate((prevLimiter) =>prevLimiter + 20)
-  //       console.log("You reached the bottom of the page!",  limiter);
-  //     }
-  //   };
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
-
-
+  let [limiter, limiterUpdate] = useState(50);
   const { docs } = UseFirestore('images', limiter);
   return (
-    <div className='masonry-container'>
+    <div  className='masonryScrollContainer'>
+    <div id='masonry' className='masonry-container'>
       {docs &&
         docs.map((obj, index) => ( // Added index as the second argument
         <div className='masonry-item' key={`masonry-item-${index}`}>
             <img src={obj.url} alt={`Image ${index + 1}`} />
           </div>
         ))}
+    </div>
     </div>
   );
 };
--- src/hooks/UseFirestore.jsx
+++ src/hooks/UseFirestore.jsx
@@ -1,16 +1,19 @@
 import {useState, useEffect}  from 'react'
 import { db, onSnapshot, orderBy, query, collection, limit } from '../firebase/config'
+import { startAfter } from 'firebase/firestore'
 const UseFirestore = (allData,  limiter) => {
     const [docs, setDocs] = useState([])
+    const [lastItem, setLastItem] = useState(null)
 
     useEffect(()=>{
         try{
-            const q = query(collection(db, allData), orderBy('timeStamps', 'desc'), limit(`${limiter}`))
+            const q = query(collection(db, allData), orderBy('timeStamps', 'desc'), limit(`${limiter}`, startAfter(lastItem || 0)))
             const unsub = onSnapshot(q, (snap) => {
              let documents = [];
              snap.forEach((doc)=>{
                  documents.push({...doc.data(), id:doc.id})
              })
+             setLastItem(documents.length-1)
              setDocs(documents)
            });
            
@@ -20,7 +23,7 @@ const UseFirestore = (allData,  limiter) => {
         }
            
 
-    },[collection])
+    },[allData])
   return {docs}
 
 
